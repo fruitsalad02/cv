cc76cc01abbf863c5770823d2e203088bd2b4e0e
fruitsalad02 <bmeimeiwang@gmail.com>
2022-01-11 23:05:01 -0500

    initial create

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Navigation from './Navigation';
+import About from './About';
+import Experience from './Experience';
+import Education from './Education';
+import Skills from './Skills';
+import Interests from './Interests';
+import Awards from './Awards';
+
+function App() {
+  return (
+    <div>
+      <body id="page-top">
+        <Navigation />
+
+        <div class="container-fluid p-0">
+          <About />
+          <Experience />
+          <Education />
+          <Skills />
+          <Interests />
+          <Awards />
+        </div>
+
+        {/* <!-- Bootstrap core JS--> */}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        {/* <!-- Core theme JS--> */}
+        <script src="js/scripts.js"></script>
+      </body>
+      {/* hi */}
+    </div>
+  );
+}
+
+export default App;
